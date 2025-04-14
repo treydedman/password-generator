4cbcee50742150baab8ab24fcdd482b7db446b29
Trey Dedman <treydedman@gmail.com>
2025-04-13 22:34:00 -0500

    fixed import error

--- client/src/components/PasswordGenerator.tsx
+++ client/src/components/PasswordGenerator.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import './PasswordGenerator.css';
 import { FaRegCopy } from 'react-icons/fa';  // Import the Clipboard Icon
 
